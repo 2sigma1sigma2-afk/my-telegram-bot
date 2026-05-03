ff0be17f0ed6eeac19a25226a8794236853daa45
2sigma1sigma2-afk <2sigma1sigma2@gmail.com>
2026-05-03 16:36:41 +0300

    Update noryxbot.py

--- noryxbot.py
+++ noryxbot.py
@@ -89,9 +89,10 @@ async def get_user_data(user_id: int, first_name: str, username: str):
 def get_main_kb(username: str, role: str):
     kb = [
         [InlineKeyboardButton(text="👤 Профиль", callback_data="profile"),
-         InlineKeyboardButton(text="🎭 Префиксы", callback_data="prefix_menu")],
+        [InlineKeyboardButton(text="🎭 Префиксы", callback_data="prefix_menu")],
         [InlineKeyboardButton(text="🔑 Активация", callback_data="activate_key"),
-         InlineKeyboardButton(text="🛒 Магазин", callback_data="buy_beta")],
+        [InlineKeyboardButton(text="📁 СКАЧАТЬ BETA", callback_data="download_beta")],
+        [InlineKeyboardButton(text="🛒 Магазин", callback_data="buy_beta")],
         [InlineKeyboardButton(text="🎁 Промокод", callback_data="promo")]
     ]
     if role in ['BETA', 'VIP']:
